efe6438f545131fc07deb16bcfe6588f29fb288a
Abdullah149081 <masudmd425@gmail.com>
2023-04-13 13:29:17 +0600

    init setup

---
+++ src/__steps.js
@@ -0,0 +1,8 @@
+/* 
+1. visit : console.firebase.google.com
+2. create project (skip google analytics)
+3 .Register app(create config)
+4.install firebase : yarn add firebase
+5. add config file add project 
+6. DANGER: Don't publish firebase config git hub
+*/
