cd8a02949357d715fa471d37938d7fb1bde9920a
Abdullah149081 <masudmd425@gmail.com>
2023-04-13 17:08:52 +0600

    login implement g

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,49 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import React, { useState } from "react";
 import { app } from "../../firebase/firebase.init";
 
 const Login = () => {
   const auth = getAuth(app);
+  const [userGet, setUserGet] = useState(null);
+  console.log(userGet);
   const provider = new GoogleAuthProvider();
   const handlerGoogleSingIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
-        console.log(user);
+        setUserGet(user);
       })
       .catch((err) => {
         console.log("error", err?.message);
       });
   };
 
+  const handlerSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        setUserGet(null);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="text-center">
-      <button className="btn" onClick={handlerGoogleSingIn}>
-        Google Login
-      </button>
+      <div>
+        {!userGet ? (
+          <button className="btn" onClick={handlerGoogleSingIn}>
+            Google Login
+          </button>
+        ) : (
+          <button onClick={handlerSignOut} className="btn">
+            Sign Out
+          </button>
+        )}
+      </div>
+      <div className="mt-6 text-2xl font-bold">
+        <h1>{userGet?.displayName}</h1>
+      </div>
     </div>
   );
 };
